552e2c3e512187c40bd09848256544e99f2e2aed
Chester Wood <chet@dewachen.org>
2019-11-01 22:43:45 -0600

    Unlink libraries

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -3,8 +3,6 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.palette.RNPalettePackage;
-import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNPalettePackage(),
-            new ImagePickerPackage()
+          new MainReactPackage()
       );
     }
 
